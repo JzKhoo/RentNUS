9d7747ea6acc0d12184ee29b3890f38c45c245ac
Jing Zhi <e0773051@u.nus.edu>
2023-02-24 13:02:14 +0800

    Fixed product page routes

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
 import products from '../products'
 
 
 const ProductScreen = ({match}) => {
-    const product = products.find((p)=> p._id === match.params.id)
+    const { id } = useParams();
+    const product = products.find((p) => String(p._id) === id);
     return (
     <>
         <Link className='btn btn-light my-3' to='/'>
