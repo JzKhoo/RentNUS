efdf6f876a05e0ac3cf18f0bda34ba6a808a2c9f
andrehzh <e0725806@u.nus.edu>
2023-03-21 23:18:29 +0800

    Check Post Man + MongoDB

--- backend/server.js
+++ backend/server.js
@@ -24,3 +24,5 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 4000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+
+//NEoK2kYtOEPRQTDN
