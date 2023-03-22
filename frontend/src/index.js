50ac0d36e288247fd9b072d8811a8edbc7195c90
andrehzh <e0725806@u.nus.edu>
2023-03-22 21:47:05 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,9 +12,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
-)
+  </Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
