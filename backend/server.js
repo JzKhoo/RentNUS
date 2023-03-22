e2f336202b3033fcc15190893742d5771d16f253
Jing Zhi <e0773051@u.nus.edu>
2023-03-22 18:45:37 +0800

    Init MongoDB database connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
+
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("API is running...")
+    res.send("API is running....")
 })
 
-app.listen(4000, console.log('Server running on port 4000'))
+app.use('/api/products', productRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
-  const { id } = useParams()
-  const product = products.find((p) => String(p._id) === id)
+  const { id } = useParams(); // instead of match.params.id
+  const [product, setProduct] = useState([])
+
+  useEffect(() => {
+    const fetchProduct = async() => {
+      const { data } = await axios.get(`/api/products/${id}`)
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [match])
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
