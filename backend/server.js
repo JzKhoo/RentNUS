adefa0abd8b26583ac3837c8aa91632bed13c079
andrehzh <e0725806@u.nus.edu>
2023-03-24 15:26:33 +0800

    Reinstallation

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,30 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import colors from 'colors'
-import productRoutes from './routes/productRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get('/', (req, res) => {
-    res.send("API is running...")
-})
+app.use("/api/products", productRoutes);
 
-app.use('/api/products', productRoutes)
+app.use(notFound);
+app.use(errorHandler);
 
-app.use(notFound)
-app.use(errorHandler)
+const PORT = process.env.PORT || 4000;
 
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
