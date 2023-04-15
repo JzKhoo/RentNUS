27367814e5ec8cbf8f3afc5e8b9878fb2a95dba3
Jing Zhi <e0773051@u.nus.edu>
2023-04-15 15:50:54 +0800

    Added view orders admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,8 +1,8 @@
-import asyncHandler from "express-async-handler";
-import Order from "../models/orderModel.js";
-import Item from "../models/itemModel.js";
-import User from "../models/userModel.js";
-import mongoose from "mongoose";
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
+import Item from '../models/itemModel.js'
+import User from '../models/userModel.js'
+import mongoose from 'mongoose'
 
 // @desc    Create new order
 // @route   POST /api/orders
@@ -16,14 +16,14 @@ const addOrderItems = asyncHandler(async (req, res) => {
     taxPrice,
     shippingPrice,
     totalPrice,
-  } = req.body;
+  } = req.body
 
-  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id)
 
   if (orderItems && orderItems.length === 0) {
-    res.status(400);
-    throw new Error("No order items");
-    return;
+    res.status(400)
+    throw new Error('No order items')
+    return
   } else if (user) {
     try {
       const order = new Order({
@@ -35,21 +35,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
         taxPrice,
         shippingPrice,
         totalPrice,
-      });
+      })
 
-      const createdOrder = await order.save();
+      const createdOrder = await order.save()
 
       for (const item of orderItems) {
-        await Item.updateOne({ _id: item.item }, { isOrderPlaced: true });
+        await Item.updateOne({ _id: item.item }, { isOrderPlaced: true })
       } // get item and set isorderplaced to be true.
 
-      res.status(201).json(createdOrder);
-      console.log("Successfully created Order!");
+      res.status(201).json(createdOrder)
+      console.log('Successfully created Order!')
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
-});
+})
 
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
@@ -62,12 +62,12 @@ const getOrderById = asyncHandler(async (req, res) => {
   // );
 
   if (order) {
-    res.json(order);
+    res.json(order)
   } else {
-    res.status(404);
-    throw new Error("Order not found");
+    res.status(404)
+    throw new Error('Order not found')
   }
-});
+})
 
 const updateOrder = asyncHandler(async (req, res) => {
   const {
@@ -81,8 +81,8 @@ const updateOrder = asyncHandler(async (req, res) => {
     createdAt,
     updatedAt,
     paidAt,
-    paymentResult
-  } = req.body;
+    paymentResult,
+  } = req.body
 
   // const userObjectId = new mongoose.Types.ObjectId(user);
 
@@ -104,15 +104,15 @@ const updateOrder = asyncHandler(async (req, res) => {
       },
     },
     { new: true }
-  );
+  )
 
   if (updatedOrder) {
-    res.json(updatedOrder);
+    res.json(updatedOrder)
   } else {
-    res.status(404);
-    throw new Error("Order not found");
+    res.status(404)
+    throw new Error('Order not found')
   }
-  // const order = await Order.findById(req.params.orderId);  
+  // const order = await Order.findById(req.params.orderId);
   // console.log("order: "+ order.orderItems);
 
   // const updatedOrder = await Order.updateOne(
@@ -132,78 +132,78 @@ const updateOrder = asyncHandler(async (req, res) => {
   //   res.status(404);
   //   throw new Error("Item not found");
   // }
-});
+})
 
 // @desc    Update order to paid
 // @route   GET /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id);
+  const order = await Order.findById(req.params.id)
 
   if (order) {
-    order.isPaid = true;
-    order.paidAt = Date.now();
+    order.isPaid = true
+    order.paidAt = Date.now()
     order.paymentResult = {
       id: req.body.id,
       status: req.body.status,
       update_time: req.body.update_time,
       email_address: req.body.payer.email_address,
-    };
+    }
     // payment result is from paypal
 
-    const updatedOrder = await order.save();
+    const updatedOrder = await order.save()
 
-    res.json(updatedOrder);
+    res.json(updatedOrder)
   } else {
-    res.status(404);
-    throw new Error("Order not found");
+    res.status(404)
+    throw new Error('Order not found')
   }
-});
+})
 
 // @desc    Update order to delivered
 // @route   GET /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id);
+  const order = await Order.findById(req.params.id)
 
   if (order) {
-    order.isDelivered = true;
-    order.deliveredAt = Date.now();
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
 
-    const updatedOrder = await order.save();
+    const updatedOrder = await order.save()
 
-    res.json(updatedOrder);
+    res.json(updatedOrder)
   } else {
-    res.status(404);
-    throw new Error("Order not found");
+    res.status(404)
+    throw new Error('Order not found')
   }
-});
+})
 
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({ user: req.user._id });
-  res.json(orders);
-});
+  const orders = await Order.find({ user: req.user._id })
+  res.json(orders)
+})
 
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
 
 const getMyItemOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({ 'orderItems.owner': req.params.userId });
-  res.json(orders);
-});
+  const orders = await Order.find({ 'orderItems.owner': req.params.userId })
+  res.json(orders)
+})
 
 // @desc    Get all orders
 // @route   GET /api/orders
 // @access  Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({}).populate("user", "id name");
-  res.json(orders);
-});
+  const orders = await Order.find({}).populate('user', 'id name')
+  res.json(orders)
+})
 
 export {
   addOrderItems,
@@ -214,4 +214,4 @@ export {
   getOrders,
   updateOrder,
   getMyItemOrders,
-};
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,6 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 /*import {
   itemListReducer,
@@ -13,12 +13,27 @@ import {
   itemDetailsReducer,
   itemListReducer,
   itemMyListReducer,
-  itemUpdateReducer
-} from "./reducers/itemReducers";
+  itemUpdateReducer,
+} from './reducers/itemReducers'
 // import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer, userUpdateReducer } from './reducers/userReducers'
-import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer, orderItemListReducer, } from "./reducers/orderReducers";
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+  userListReducer,
+  userDeleteReducer,
+  userUpdateReducer,
+} from './reducers/userReducers'
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+  orderListMyReducer,
+  orderItemListReducer,
+  orderListReducer,
+} from './reducers/orderReducers'
 
 const reducer = combineReducers({
   itemList: itemListReducer,
@@ -35,29 +50,29 @@ const reducer = combineReducers({
   userList: userListReducer,
   userDelete: userDeleteReducer,
   userUpdate: userUpdateReducer,
-  orderCreate : orderCreateReducer,
-  orderDetails : orderDetailsReducer,
-  orderPay : orderPayReducer,
-  orderListMy : orderListMyReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  orderListMy: orderListMyReducer,
   orderItemList: orderItemListReducer,
-  // itemListMy : itemListMyReducer,
-});
+  orderList: orderListReducer,
+})
 
-const cartItemsFromStorage = localStorage.getItem("cartItems")
-  ? JSON.parse(localStorage.getItem("cartItems"))
-  : [];
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
 
-const itemInfoFromStorage = localStorage.getItem("itemInfo")
-  ? JSON.parse(localStorage.getItem("itemInfo"))
-  : [];
+const itemInfoFromStorage = localStorage.getItem('itemInfo')
+  ? JSON.parse(localStorage.getItem('itemInfo'))
+  : []
 
-const userInfoFromStorage = localStorage.getItem("userInfo")
-  ? JSON.parse(localStorage.getItem("userInfo"))
-  : null;
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
 
-const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
-  ? JSON.parse(localStorage.getItem("shippingAddress"))
-  : {};
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {}
 
 const initialState = {
   cart: {
@@ -66,14 +81,14 @@ const initialState = {
   },
   userLogin: { userInfo: userInfoFromStorage },
   itemAdd: { itemInfo: itemInfoFromStorage },
-};
+}
 
-const middleware = [thunk];
+const middleware = [thunk]
 
 const store = createStore(
   reducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
-);
+)
 
-export default store;
+export default store
